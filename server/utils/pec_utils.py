dc52ad2eab1eca0bdfe35e3471d7b57011cf372e
CharlieJin <72548752+ZiningJin@users.noreply.github.com>
2024-02-06 15:51:07 +0800

    Update pec_utils.py

--- server/utils/pec_utils.py
+++ server/utils/pec_utils.py
@@ -19,7 +19,7 @@ def pec_util_login(url, username, password):
         WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.NAME, 'loginuser'))).send_keys(username)
         browser.find_element(By.NAME, 'loginpassword').send_keys(password)
 
-        login_button_xpath = '//*[@id="content"]/main/section/div/div/div/form/div[4]/input'
+        login_button_xpath = ''
         WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH, login_button_xpath))).click()
         # Return the browser object for the next step
         return browser
@@ -34,7 +34,7 @@ def pec_util_auth_code(browser, auth_code):
         print("Browser is not intialized")
         return
     try:
-        authcode_input_xpath = '//*[@id="vCode"]'
+        authcode_input_xpath = ''
         WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.XPATH, authcode_input_xpath))).send_keys(auth_code)
         validate_button_xpath = '//*[@id="vCode-btn"]'
         WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH, validate_button_xpath))).click()
@@ -53,12 +53,12 @@ def pec_util_criteria(browser, start_date_str, end_date_str):
 
     try:
         # Click various criteria 
-        capital_click_xpath = '//*[@id="CHECKCAPMAINPROJECTSCap"]'
-        newProjects_click_xpath = '/html/body/div[2]/section[2]/form/div[4]/fieldset[2]/div[2]/div[2]/div[2]/fieldset[2]/div[1]/div[1]/label[1]/input'
-        projectStatusActive_click_xpath = '//*[@id="divpwsfg_Project_NameStatusValue"]/div[2]/div[1]/fieldset[3]/div/div[1]/select/option[1]'
-        projectStatusUnconfirmed_click_xpath = '//*[@id="divpwsfg_Project_NameStatusValue"]/div[2]/div[1]/fieldset[3]/div/div[1]/select/option[6]'
-        worldregion_click_xpath = '//*[@id="divpwsfg_Project_Geography"]/div[2]/fieldset/div[2]/div[1]/div[1]/div[3]/input[1]'
-        traderegion_click_xpath = '//*[@id="divpwsfg_Project_TradingRegions"]/div[3]/div[4]/label[3]/input'
+        capital_click_xpath = '/'
+        newProjects_click_xpath = ''
+        projectStatusActive_click_xpath = ''
+        projectStatusUnconfirmed_click_xpath = ''
+        worldregion_click_xpath = ''
+        traderegion_click_xpath = ''
 
 
         WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.XPATH, capital_click_xpath))).click()
@@ -69,16 +69,16 @@ def pec_util_criteria(browser, start_date_str, end_date_str):
         WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.XPATH, traderegion_click_xpath))).click()
         
         projectindustry_xpath_dict = {
-            'power':'//*[@id="divpwsfg_Project_IndustrySic"]/fieldset[1]/div/div[1]/div[1]/input',
-            'productOG':'//*[@id="divpwsfg_Project_IndustrySic"]/fieldset[1]/div/div[2]/div[1]/input',
-            'alternativefuel':'//*[@id="divpwsfg_Project_IndustrySic"]/fieldset[1]/div/div[2]/div[2]/input',
-            'petrolrefine': '//*[@id="divpwsfg_Project_IndustrySic"]/fieldset[1]/div/div[2]/div[3]/input',
-            'chemicalprocess': '//*[@id="divpwsfg_Project_IndustrySic"]/fieldset[1]/div/div[3]/div[1]/input',
-            'foodandbever': '//*[@id="divpwsfg_Project_IndustrySic"]/fieldset[1]/div/div[4]/div[1]/input',
-            'metalandmineral': '//*[@id="divpwsfg_Project_IndustrySic"]/fieldset[1]/div/div[3]/div[2]/input',
-            'industryandmanufacture': '//*[@id="divpwsfg_Project_IndustrySic"]/fieldset[1]/div/div[4]/div[2]/input',
-            'pulpandpaper': '//*[@id="divpwsfg_Project_IndustrySic"]/fieldset[1]/div/div[3]/div[3]/input',
-            'pharandmedical': '//*[@id="divpwsfg_Project_IndustrySic"]/fieldset[1]/div/div[4]/div[3]/input'
+            'power':'',
+            'productOG':'',
+            'alternativefuel':'',
+            'petrolrefine': '',
+            'chemicalprocess': '',
+            'foodandbever': '',
+            'metalandmineral': '',
+            'industryandmanufacture': '',
+            'pulpandpaper': '',
+            'pharandmedical': ''
         }
 
         for i in projectindustry_xpath_dict.keys():
@@ -89,7 +89,7 @@ def pec_util_criteria(browser, start_date_str, end_date_str):
         browser.execute_script('document.getElementsByClassName("datepicker_dMY form-control input-xs ui-corner-all")[1].value=arguments[0];', end_date_str)
 
         # click the 'run query' button
-        run_query_button_xpath = '//*[@id="top_dash"]/div[1]/div[1]/input[3]'
+        run_query_button_xpath = ''
         WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.XPATH, run_query_button_xpath))).click()
         
         # Return the browser object for the next step
@@ -127,8 +127,8 @@ def pec_util_extract(browser):
             for i in range(1, num_rows, 2):
                 # extact projectID, projectURL, countryZone
                 projectID_xpath = f'//*[@id="DataTables_Table_0"]/tbody/tr[{i}]/td[4]'
-                projectIndustry_xpath = f'//*[@id="DataTables_Table_0"]/tbody/tr[1]/td[3]'
-                countryZone_xpath = f'//*[@id="DataTables_Table_0"]/tbody/tr[{i}]/td[9]'
+                projectIndustry_xpath = f''
+                countryZone_xpath = f''
 
                 projectID = browser.find_element(By.XPATH, projectID_xpath).text
                 projectIndustry = browser.find_element(By.XPATH, projectIndustry_xpath).text
@@ -162,28 +162,28 @@ def pec_util_extract(browser):
 
             # Extract projectName
             try:
-                projectName = browser.find_element(By.XPATH, "//b[text()='Project Name']/../following-sibling::td[1]/span").text
+                projectName = browser.find_element(By.XPATH, "").text
             except NoSuchElementException:
                 XXX = None
             projectINFOs[projectID]['projectName'] = projectName
 
             # Extract projectType
             try:
-                projectType  = browser.find_element(By.XPATH, "//b[text()='Project Type']/../following-sibling::td[1]/span").text
+                projectType  = browser.find_element(By.XPATH, "").text
             except NoSuchElementException:
                 projectType  = None
             projectINFOs[projectID]['projectType '] = projectType
 
             # Extract plantOwner
             try:
-                plantOwner = browser.find_element(By.XPATH, "//b[text()='Plant Owner']/../following-sibling::td[1]/span").text
+                plantOwner = browser.find_element(By.XPATH, "").text
             except NoSuchElementException:
                 plantOwner = None
             projectINFOs[projectID]['plantOwner'] = plantOwner
 
             # Extract location
             try:
-                location = browser.find_element(By.XPATH, "//b[text()='Location']/../following-sibling::td[1]").text
+                location = browser.find_element(By.XPATH, "").text
             except NoSuchElementException:
                 location = None
             projectINFOs[projectID]['location'] = location
@@ -192,7 +192,7 @@ def pec_util_extract(browser):
             cityState = None
             for cityState_title in ['City/State','Field Name/Water Body']:
                 try:
-                    cityState = browser.find_element(By.XPATH, f"//b[text()='{cityState_title}']/../following-sibling::td[1]").text
+                    cityState = browser.find_element(By.XPATH, f"").text
                     break
                 except NoSuchElementException:
                     continue
@@ -203,12 +203,12 @@ def pec_util_extract(browser):
             for contact_title in ['Project Manager', 'Project Executive', 'Project Director']:
                 try:
                     fullContacts1 = browser.find_element(By.XPATH, f"//b[text()='{contact_title}']/../../following-sibling::td[1]").text 
-                    fullContacts2 = browser.find_element(By.XPATH, f"//b[text()='{contact_title}']/../../following-sibling::td[2]").text 
-                    fullContacts3 = browser.find_element(By.XPATH, f"//b[text()='{contact_title}']/../../../following-sibling::tr[2]/td[2]").text 
-                    fullContacts4 = browser.find_element(By.XPATH, f"//b[text()='{contact_title}']/../../../following-sibling::tr[3]/td[2]").text 
-                    fullContacts5 = browser.find_element(By.XPATH, f"//b[text()='{contact_title}']/../../../following-sibling::tr[5]").text 
-                    fullContacts6 = browser.find_element(By.XPATH, f"//b[text()='{contact_title}']/../../../following-sibling::tr[7]/td[2]").text 
-                    fullContacts7 = browser.find_element(By.XPATH, f"//b[text()='{contact_title}']/../../../following-sibling::tr[8]/td[2]").text 
+                    fullContacts2 = browser.find_element(By.XPATH, f"").text 
+                    fullContacts3 = browser.find_element(By.XPATH, f"").text 
+                    fullContacts4 = browser.find_element(By.XPATH, f"").text 
+                    fullContacts5 = browser.find_element(By.XPATH, f"").text 
+                    fullContacts6 = browser.find_element(By.XPATH, f"").text 
+                    fullContacts7 = browser.find_element(By.XPATH, f"]").text 
                     fullContacts = 'Project Manager:'+fullContacts1+fullContacts2+';'+fullContacts3+fullContacts4+';'+fullContacts5+fullContacts6+fullContacts7
                     break
                 except NoSuchElementException:
@@ -217,28 +217,28 @@ def pec_util_extract(browser):
 
             # Extract scope
             try:
-                scope = browser.find_element(By.XPATH, "//b[text()='Scope']/../following-sibling::td[1]").text
+                scope = browser.find_element(By.XPATH, "").text
             except NoSuchElementException:
                 scope = None
             projectINFOs[projectID]['scope'] = scope
 
             # Extract schedule
             try:
-                schedule = browser.find_element(By.XPATH, "//b[text()='Schedule']/../following-sibling::td[1]").text
+                schedule = browser.find_element(By.XPATH, "").text
             except NoSuchElementException:
                 schedule = None
             projectINFOs[projectID]['schedule'] = schedule
 
         # Extract contactName
             try:
-                contactName = browser.find_element(By.XPATH, "//b[text()='QC Date/LinkedIn ID']/../../following-sibling::tr[1]/td[2]/span").text
+                contactName = browser.find_element(By.XPATH, "").text
             except NoSuchElementException:
                 contactName = None
             projectINFOs[projectID]['contactName'] = contactName
 
         # Extract contactPhone
             try:
-                contactPhone = browser.find_element(By.XPATH, "//b[text()='QC Date/LinkedIn ID']/../../following-sibling::tr[1]/td[3]/span").text
+                contactPhone = browser.find_element(By.XPATH, "").text
             except NoSuchElementException:
                 contactPhone = None
             projectINFOs[projectID]['contactPhone'] = contactPhone
